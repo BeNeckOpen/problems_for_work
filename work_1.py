d8fcc16ddc65bfd2f5dd7af0f37b3690f9f90128
BeNeckOpen <sasha.lorens@yandex.ru>
2020-02-17 02:12:24 +0300

    Update work_1.py

--- work_1.py
+++ work_1.py
@@ -1,4 +1,5 @@
 def file_pars():
+    sum = 0
     all_sum = 0
     i = 1
     f = open('names.txt', 'r')
@@ -9,8 +10,9 @@ def file_pars():
     word.sort()
     for one_word in word:
         for letter in one_word:
-            sum = ord(letter) - 64
+            sum = ord(letter) - 64 + sum
         all_sum = i * sum + all_sum
+        sum = 0
         i = i + 1
     print(all_sum)
 
