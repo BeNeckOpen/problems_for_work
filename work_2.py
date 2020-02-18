7a536fe48ed04e02e4e22fe56a2fb9d5ca9b5243
BeNeckOpen <sasha.lorens@yandex.ru>
2020-02-19 02:13:17 +0300

    Update work_2.py

--- work_2.py
+++ work_2.py
@@ -1,10 +1,10 @@
 def file_pars():
     f = open('hits.txt', 'r')
-    dict = {}
+    dict_standart = {}
     ip_s = [ip_s.split().pop(1) for ip_s in f]
-    for ip in reversed(ip_s):
-        dict[ip] = dict.get(ip, 0) + 1
-    list_d = list(dict.items())
+    for ip in ip_s:
+        dict_standart[ip] = dict_standart.get(ip, 0) + 1
+    list_d = list(dict_standart.items())
     list_d.sort(key=lambda i: i[1])
     for i in range(5):
         print(list_d[len(list_d) - i - 1][0])
